bc6d61c17922942c54f79ef27726c92b7ef606b0
Taylor Seager <taylorseager718@gmail.com>
2024-05-20 18:39:47 -0500

    forms directory and category/recipe form files created

---
+++ components/forms/CategoryForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+function CategoryForm() {
+  return (
+    <div>CategoryForm</div>
+  );
+}
+
+CategoryForm.propTypes = {};
+
+export default CategoryForm;
