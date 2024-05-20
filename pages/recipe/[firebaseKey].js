c1a921521202fcd5651a4535d60c1ad65dbe330d
Taylor Seager <taylorseager718@gmail.com>
2024-05-20 18:55:20 -0500

    recipe directory, edit subdirectory, d2 dynamic files and newRecipe.js created

---
+++ pages/recipe/[firebaseKey].js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function ViewRecipeDetails() {
+  return (
+    <div>This will render the recipe details page</div>
+  );
+}
---
+++ pages/recipe/edit/[firebaseKey].js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function EditRecipe() {
+  return (
+    <div>This will be the update recipe form.</div>
+  );
+}
