eac4bd3b10c150dcbaa00505fc22562c792ffafe
Taylor Seager <taylorseager718@gmail.com>
2024-05-20 19:03:18 -0500

    href updated for nav links

--- components/NavBarAuth.js
+++ components/NavBarAuth.js
@@ -11,7 +11,7 @@ export default function NavBarAuth() {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Link passHref href="/">
-          <Navbar.Brand>CHANGE ME</Navbar.Brand>
+          <Navbar.Brand>Culinary Chronicles</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -19,6 +19,15 @@ export default function NavBarAuth() {
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
             <Link passHref href="/">
               <Nav.Link>Home</Nav.Link>
+            </Link>
+            <Link passHref href="/recipe">
+              <Nav.Link>Recipes</Nav.Link>
+            </Link>
+            <Link passHref href="/category">
+              <Nav.Link>Categories</Nav.Link>
+            </Link>
+            <Link passHref href="/recipe/newRecipe">
+              <Nav.Link>Create Recipe</Nav.Link>
             </Link>
             <Button variant="danger" onClick={signOut}>Sign Out</Button>
           </Nav>
