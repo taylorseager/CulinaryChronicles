b436434f7e097dfed2a3981f73eeb129f779f527
Taylor Seager <taylorseager718@gmail.com>
2024-05-20 20:19:21 -0500

    home button removed from navBar since logo/name will be home button

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,11 +1,11 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import Link from 'next/link';
 import {
-  Navbar, Container, Nav,
+  Navbar, Container, Nav, Button,
 } from 'react-bootstrap';
+import { signOut } from '../utils/auth';
 
-export default function NoAuthNavBar() {
+export default function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
@@ -16,9 +16,16 @@ export default function NoAuthNavBar() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
-            <Link passHref href="/">
-              <Nav.Link>Home</Nav.Link>
+            <Link passHref href="/recipe">
+              <Nav.Link>Recipes</Nav.Link>
             </Link>
+            <Link passHref href="/category">
+              <Nav.Link>Categories</Nav.Link>
+            </Link>
+            <Link passHref href="/recipe/newRecipe">
+              <Nav.Link>Create Recipe</Nav.Link>
+            </Link>
+            <Button variant="danger" onClick={signOut}>Sign Out</Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
