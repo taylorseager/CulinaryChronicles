0a2fe924a6dc294c6b7a03f18037f008d90c91a1
Taylor Seager <taylorseager718@gmail.com>
2024-05-20 21:20:58 -0500

    recipe card renders to correct route; not dynamically rendered at this point

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export default function RecipeCard() {
   return (
-    <div>RecipeCard</div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Recipe Name:</Card.Title>
+        <Card.Text>
+          <p>Total Time:</p>
+          <p>Servings: Dropdown</p>
+          <p>Category:</p>
+          <p>Description:</p>
+        </Card.Text>
+        <Button variant="primary">View</Button>
+        <Button variant="secondary">Edit</Button>
+        <Button variant="danger">Delete</Button>
+      </Card.Body>
+    </Card>
   );
 }
