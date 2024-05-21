e90d023b35d61c95eb5f8acaeece6213851f96b6
Taylor Seager <taylorseager718@gmail.com>
2024-05-21 18:37:33 -0500

    getRecipes created, checked in postman, working as expected

--- api/recipeData.js
+++ api/recipeData.js
@@ -1,1 +1,18 @@
-<div>This will hold the promises for the recipe data</div>;
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+// Pull all recipes from firebase
+const getRecipes = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/Recipes.json?orderBy="uid"&equalTo="${uid}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+export default getRecipes;
