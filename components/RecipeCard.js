ef18bcd9ba7df691f4a5b55672aa375aa0685afe
Taylor Seager <taylorseager718@gmail.com>
2024-05-20 21:45:01 -0500

    recipe card updated to dynamically rendered, need to add api calls and map over recipe cards

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,18 +1,25 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import PropTypes from 'prop-types';
+// import { useRouter } from 'next/router';
 
-export default function RecipeCard() {
+export default function RecipeCard({ recipeObj }) {
+  // const router = useRouter();
+  // const { firebaseKey } = router.query;
+
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      {/* <Card.Img src={recipeObj.image} alt={recipeObj.title} style={{ height: '250px' }} /> */}
       <Card.Body>
-        <Card.Title>Recipe Name:</Card.Title>
+        <Card.Title>Recipe Name:{recipeObj.title}</Card.Title>
         <Card.Text>
-          <p>Total Time:</p>
-          <p>Servings: Dropdown</p>
-          <p>Category:</p>
-          <p>Description:</p>
+          <p>Total Time: {recipeObj.totalTime} (mins)</p>
+          <p>Servings: {recipeObj.servings}</p>
+          <p>Category: {recipeObj.categoryId}</p>
+          <p>Description: {recipeObj.description}</p>
+          <p>Favorite: {recipeObj.favorite}</p>
         </Card.Text>
         <Button variant="primary">View</Button>
         <Button variant="secondary">Edit</Button>
@@ -21,3 +28,15 @@ export default function RecipeCard() {
     </Card>
   );
 }
+
+RecipeCard.propTypes = {
+  recipeObj: PropTypes.shape({
+    image: PropTypes.string,
+    title: PropTypes.string,
+    servings: PropTypes.number,
+    totalTime: PropTypes.number,
+    categoryId: PropTypes.string,
+    description: PropTypes.string,
+    favorite: PropTypes.string,
+  }).isRequired,
+};
