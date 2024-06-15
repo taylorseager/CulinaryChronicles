4797d06a35d1018764653bec19162a6b1415901f
Taylor Seager <taylorseager718@gmail.com>
2024-06-15 10:11:43 -0500

    create, update and delete category api calls created

--- api/categoryData.js
+++ api/categoryData.js
@@ -14,4 +14,52 @@ const getCategories = () => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
-export default getCategories;
+const createNewCategory = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/Categories.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateCategory = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/Categories/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve((data)))
+    .catch(reject);
+});
+
+const deleteCategory = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/Categories/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        resolve();
+      } else {
+        reject(new Error('Failed to delete category'));
+      }
+    })
+    .catch(reject);
+});
+
+export {
+  getCategories,
+  createNewCategory,
+  updateCategory,
+  deleteCategory,
+};
